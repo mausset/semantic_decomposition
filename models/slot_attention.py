ff475eda2b70efc65191d06277fd359197e01add
riko <rickard.maus@gmail.com>
2024-02-16 11:13:15 +0100

    Revert "add stuff"
    This reverts commit 737d41c7650edef254f1df58b85cdd0a6b875f75.

--- models/slot_attention.py
+++ models/slot_attention.py
@@ -1,117 +1,180 @@
 import lightning as pl
 import torch
 from torch import nn
-from einops import rearrange
+from torch.nn import functional as F
+from einops import rearrange, repeat
 
 from x_transformers import Encoder
 
 
-class SA(pl.LightningModule):
+class ISA(pl.LightningModule):
 
     def __init__(
-        self, input_dim, slot_dim, n_slots=7, n_iters=3, implicit=False, eps=1e-8
+        self, input_dim, slot_dim, n_iters, n_slots=5, min_slots=None, eps=1e-8
     ):
         super().__init__()
+
         self.in_dim = input_dim
         self.slot_dim = slot_dim
-        self.n_slots = n_slots
         self.n_iters = n_iters
-        self.implicit = implicit
+        self.n_slots = n_slots
+        self.min_slots = min_slots
         self.eps = eps
 
-        self.scale = input_dim**-0.5
+        if self.min_slots is not None:
+            assert self.min_slots <= self.n_slots
 
         self.mu = nn.Parameter(torch.randn(slot_dim))
-        self.slots_logsigma = nn.Parameter(torch.zeros(1, 1, slot_dim))
-        nn.init.xavier_uniform_(self.slots_logsigma)
-        self.slots_logsigma = nn.Parameter(self.slots_logsigma.squeeze())
+        self.sigma = nn.Parameter(torch.randn(slot_dim))
 
-        self.inv_cross_k = nn.Linear(input_dim, slot_dim, bias=False)
-        self.inv_cross_v = nn.Linear(input_dim, slot_dim, bias=False)
-        self.inv_cross_q = nn.Linear(slot_dim, slot_dim, bias=False)
+        self.in_to_kv = nn.Linear(input_dim, slot_dim * 2, bias=False)
+        self.slot_to_q = nn.Linear(slot_dim, slot_dim, bias=False)
 
         self.gru = nn.GRUCell(slot_dim, slot_dim)
-
-        self.mlp = nn.Sequential(
+        self.ffn = nn.Sequential(
             nn.Linear(slot_dim, slot_dim * 2),
-            nn.ReLU(inplace=True),
+            nn.GELU(),
             nn.Linear(slot_dim * 2, input_dim),
         )
 
-        self.norm_input = nn.LayerNorm(slot_dim)
+        self.input_norm = nn.LayerNorm(input_dim)
         self.norm_slots = nn.LayerNorm(slot_dim)
-        self.norm_pre_ff = nn.LayerNorm(slot_dim)
-
-        self.norm_recurrent = nn.LayerNorm(slot_dim)
+        self.norm_ffn = nn.LayerNorm(slot_dim)
 
     def step(self, slots, k, v):
         _, n, _ = slots.shape
 
-        q = self.inv_cross_q(self.norm_slots(slots))
+        q = self.slot_to_q(self.norm_slots(slots))
+        attn = F.softmax(torch.einsum("bkd,bqd->bkq", k, q), dim=-1)
+        attn = attn / torch.sum(attn + self.eps, dim=-2, keepdim=True)
+        updates = torch.einsum("bvq,bvd->bqd", attn, v)
 
-        dots = torch.einsum("bid,bjd->bij", q, k) * self.scale
-        attn = dots.softmax(dim=1) + self.eps
-
-        attn = attn / attn.sum(dim=-1, keepdim=True)
-
-        updates = torch.einsum("bjd,bij->bid", v, attn)
-        slots = rearrange(slots, "b n d -> (b n) d")
-        updates = rearrange(updates, "b n d -> (b n) d")
+        # TODO: I hate this
+        updates = rearrange(updates, "bt n d -> (bt n) d")
+        slots = rearrange(slots, "bt n d -> (bt n) d")
         slots = self.gru(updates, slots)
-        slots = rearrange(slots, "(b n) d -> b n d", n=n)
+        updates = rearrange(updates, "(bt n) d -> bt n d", n=n)
+        slots = rearrange(slots, "(bt n) d -> bt n d", n=n)
 
-        slots = slots + self.mlp(self.norm_pre_ff(slots))
+        slots = slots + self.ffn(self.norm_ffn(slots))
 
         return slots
 
-    def forward(self, x, sample):
-        b, t, _, _ = x.shape
-
-        if sample is None:
-            sample = torch.randn((b, t, self.n_slots, self.slot_dim), device=x.device)
-        slots = self.mu + self.slots_logsigma.exp() * sample
-
-        k = self.inv_cross_k(x)
-        v = self.inv_cross_v(x)
-
-        k = rearrange(k, "b t n d -> (b t) n d")
-        v = rearrange(v, "b t n d -> (b t) n d")
-        slots = rearrange(slots, "b t n d -> (b t) n d")
-
-        for _ in range(self.n_iters):
-            slots = self.step(slots, k, v)
-
-        if self.implicit:
-            slots = self.step(slots.detach(), k, v)
-
-        slots = rearrange(slots, "(b t) n d -> b t n d", t=t)
-
-        return slots, sample
-
-    def recurrent(self, x, init_slots):
-        b, t, _, _ = x.shape
+    def forward(self, x, init_slots=None, time=1):
+        n_slots = self.n_slots
+        if self.min_slots is not None:
+            n_slots = torch.randint(self.min_slots, self.n_slots + 1).item()
 
         if init_slots is None:
-            sample = torch.randn((b, self.n_slots, self.slot_dim), device=x.device)
-            init_slots = self.mu + self.slots_logsigma.exp() * sample
-        slots = init_slots
+            init_slots = self.mu + self.sigma * torch.randn(
+                (x.shape[0] // time, n_slots, self.slot_dim), device=x.device
+            )
+            init_slots = repeat(init_slots, "b n d -> b t n d", t=time)
 
-        k = self.inv_cross_k(x)
-        v = self.inv_cross_v(x)
+        init_slots = rearrange(init_slots, "b t n d -> (b t) n d")
+        slots = init_slots.clone()
+
+        x = self.input_norm(x)
+        k, v = self.in_to_kv(x).chunk(2, dim=-1)
+        k = k * (self.slot_dim**-0.5)
+
+        for _ in range(self.n_iters - 1):
+            slots = self.step(slots, k, v)
+        slots = self.step(slots.detach(), k, v)
+
+        slots = rearrange(slots, "(b t) n d -> b t n d", t=time)
+        init_slots = rearrange(init_slots, "(b t) n d -> b t n d", t=time)
+
+        return slots, init_slots.detach()
+
+
+class ISARecurrent(pl.LightningModule):
+
+    def __init__(
+        self, input_dim, slot_dim, n_iters, n_slots=5, min_slots=None, eps=1e-8
+    ):
+        super().__init__()
+
+        self.in_dim = input_dim
+        self.slot_dim = slot_dim
+        self.n_iters = n_iters
+        self.n_slots = n_slots
+        self.min_slots = min_slots
+        self.eps = eps
+
+        if self.min_slots is not None:
+            assert self.min_slots <= self.n_slots
+
+        self.mu = nn.Parameter(torch.randn(slot_dim))
+        self.sigma = nn.Parameter(torch.randn(slot_dim))
+
+        self.in_to_kv = nn.Linear(input_dim, slot_dim * 2, bias=False)
+        self.slot_to_q = nn.Linear(slot_dim, slot_dim, bias=False)
+
+        self.gru = nn.GRUCell(slot_dim, slot_dim)
+        self.ffn = nn.Sequential(
+            nn.Linear(slot_dim, slot_dim * 2),
+            nn.GELU(),
+            nn.Linear(slot_dim * 2, input_dim),
+        )
+
+        self.input_norm = nn.LayerNorm(input_dim)
+        self.norm_slots = nn.LayerNorm(slot_dim)
+        self.norm_pre_ffn = nn.LayerNorm(slot_dim)
+        self.norm_post_ffn = nn.LayerNorm(slot_dim)
+
+    def step(self, slots, k, v):
+        _, n, _ = slots.shape
+
+        q = self.slot_to_q(self.norm_slots(slots))
+        attn = F.softmax(torch.einsum("bkd,bqd->bkq", k, q), dim=-1)
+        attn = attn / torch.sum(attn + self.eps, dim=-2, keepdim=True)
+        updates = torch.einsum("bvq,bvd->bqd", attn, v)
+
+        # TODO: I hate this
+        updates = rearrange(updates, "b n d -> (b n) d")
+        slots = rearrange(slots, "b n d -> (b n) d")
+        slots = self.gru(updates, slots)
+        updates = rearrange(updates, "(b n) d -> b n d", n=n)
+        slots = rearrange(slots, "(b n) d -> b n d", n=n)
+
+        slots = self.norm_post_ffn(slots + self.ffn(self.norm_pre_ffn(slots)))
+
+        return slots
+
+    def iterate(self, slots, k, v):
+        for _ in range(self.n_iters - 1):
+            slots = self.step(slots, k, v)
+        slots = self.step(slots.detach(), k, v)
+        return slots
+
+    def forward(self, x, init_slots=None):
+        _, t, _, _ = x.shape
+
+        n_slots = self.n_slots
+        if self.min_slots is not None:
+            n_slots = torch.randint(self.min_slots, self.n_slots + 1).item()
+
+        if init_slots is None:
+            init_slots = self.mu + self.sigma * torch.randn(
+                (x.shape[0], n_slots, self.slot_dim), device=x.device
+            )
+
+        slots = init_slots.clone()
+
+        x = self.input_norm(x)
+        k, v = self.in_to_kv(x).chunk(2, dim=-1)
+        k = k * (self.slot_dim**-0.5)
 
         results = []
         for i in range(t):
-            for _ in range(self.n_iters):
-                slots = self.step(slots, k[:, i], v[:, i])
-            if self.implicit:
-                slots = self.step(slots.detach(), k[:, i], v[:, i])
-            slots = self.norm_recurrent(slots)
+            slots = self.iterate(slots, k[:, i], v[:, i])
             results.append(slots)
-            slots = slots.detach()
+            slots = slots.detach()  # Maybe not necessary
 
         slots = torch.stack(results, dim=1)
 
-        return slots, None
+        return slots, init_slots.detach()
 
 
 class ICASALayer(pl.LightningModule):
@@ -175,6 +238,21 @@ class ICASA(pl.LightningModule):
         )
 
     def forward(self, x, sample):
+        b, t, _, _ = x.shape
+
+        if sample is None:
+            sample = torch.randn((b, t, self.n_slots, self.slot_dim), device=x.device)
+        slots = self.mu + self.slots_logsigma.exp() * sample
+
+        slots = rearrange(slots, "b t n d -> (b t) n d")
+        x = rearrange(x, "b t n d -> (b t) n d")
+        for layer in self.layers:
+            slots = layer(x, slots)
+        slots = rearrange(slots, "(b t) n d -> b t n d", b=b)
+
+        return slots, sample
+
+    def recurrent(self, x, sample):
 
         if sample is None:
             sample = torch.randn(
@@ -191,10 +269,3 @@ class ICASA(pl.LightningModule):
 
         slots = torch.stack(results, dim=1)
         return slots, sample
-
-    def non_recurrent(self, x, slots):
-
-        for layer in self.layers:
-            slots = layer(x, slots)
-
-        return slots
