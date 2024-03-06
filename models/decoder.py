3dc33d1cf2d318e3a849a31071000f3e54e17849
riko <rickard.maus@gmail.com>
2024-03-06 15:59:52 +0100

    add mlp decoder

--- models/decoder.py
+++ models/decoder.py
@@ -6,6 +6,56 @@ from torch.nn import ConvTranspose2d, functional as F
 from x_transformers import Encoder, ContinuousTransformerWrapper
 
 
+class MLPDecoder(pl.LightningModule):
+
+    def __init__(self, dim, depth, resolution, expansion_factor=2) -> None:
+        super().__init__()
+
+        self.dim = dim
+        self.depth = depth
+        self.resolution = resolution
+
+        self.pos_enc = FourierPositionalEncoding(in_dim=2, out_dim=dim)
+
+        mlp = []
+        for i in range(depth - 1):
+            mlp.append(
+                nn.Linear(dim * expansion_factor if i else 1, dim * expansion_factor)
+            )
+            mlp.append(nn.ReLU())
+        mlp.append(nn.Linear(dim * expansion_factor, dim))
+        self.mlp = nn.Sequential(*mlp)
+
+        self.alpha_projection = nn.Linear(dim, 1)
+
+    def forward(self, x):
+        """
+        args:
+            x: (B, N, D), extracted object representations
+        returns:
+            (B, H, W), predicted masks
+        """
+
+        grid = make_grid(self.resolution, device=x.device)
+        grid = repeat(grid, "b h w d -> (b r) h w n d", r=x.shape[0], n=x.shape[1])
+
+        scaffold = self.pos_enc(grid)
+        x = repeat(x, "b n d -> b h w n d", h=self.resolution[0], w=self.resolution[1])
+        x = x + scaffold
+
+        result = self.mlp(x)
+        alpha = self.alpha_projection(result)
+        result = (result * F.softmax(alpha, dim=1)).sum(dim=1)
+        result = rearrange(
+            result,
+            "(b h w) d -> b h w d",
+            h=self.resolution[0],
+            w=self.resolution[1],
+        )
+
+        return result
+
+
 class TransformerDecoder(pl.LightningModule):
 
     def __init__(self, dim, depth, resolution) -> None:
