d772fe215ed96be1ef72bd4c516bf057bbe0db54
riko <rickard.maus@gmail.com>
2024-04-30 12:23:54 +0200

    add slot attention stuff

--- models/slot_attention.py
+++ models/slot_attention.py
@@ -14,8 +14,8 @@ def build_slot_attention(arch: str, args):
             return SA(**args)
         case "sat":
             return SAT(**args)
-        case "psat":
-            return PSAT(**args)
+        case "satv2":
+            return SATv2(**args)
         case _:
             raise ValueError(f"Unknown slot attention architecture: {arch}")
 
@@ -150,23 +150,12 @@ class SAT(nn.Module):
         self.inv_cross_v = nn.Linear(input_dim, slot_dim, bias=ica_bias)
         self.inv_cross_q = nn.Linear(slot_dim, slot_dim, bias=ica_bias)
 
-        self.t_encoder = nn.ModuleList(
-            [
-                Encoder(
-                    dim=slot_dim,
-                    depth=1,
-                    ff_glu=True,
-                    ff_swish=True,
-                )
-                for _ in range(depth)
-            ]
+        self.t_encoder = Encoder(
+            dim=slot_dim,
+            depth=depth,
+            ff_glu=True,
+            ff_swish=True,
         )
-        # self.t_encoder = Encoder(
-        #     dim=slot_dim,
-        #     depth=depth,
-        #     ff_glu=True,
-        #     ff_swish=True,
-        # )
 
         self.norm_input = nn.LayerNorm(slot_dim)
         self.norm_slots = nn.LayerNorm(slot_dim)
@@ -200,20 +189,19 @@ class SAT(nn.Module):
 
     def step(self, slots, k, v, return_attn=False, mask=None):
 
-        for t_encoder in self.t_encoder:
-            q = self.inv_cross_q(self.norm_slots(slots))
+        q = self.inv_cross_q(self.norm_slots(slots))
 
-            updates_attn = [self.inv_cross_attn(q, ki, vi) for ki, vi in zip(k, v)]
+        updates_attn = [self.inv_cross_attn(q, ki, vi) for ki, vi in zip(k, v)]
 
-            updates = [ui for ui, _ in updates_attn]
-            attn = [attn for _, attn in updates_attn]
+        updates = [ui for ui, _ in updates_attn]
+        attn = [attn for _, attn in updates_attn]
 
-            updates = torch.stack(updates, dim=0).mean(dim=0)
+        updates = torch.stack(updates, dim=0).mean(dim=0)
 
-            slots = slots + updates
-            slots = self.norm_ica(slots)
+        slots = slots + updates
+        slots = self.norm_ica(slots)
 
-            slots = t_encoder(slots)
+        slots = self.t_encoder(slots)
 
         if return_attn:
             return slots, attn
@@ -246,7 +234,7 @@ class SAT(nn.Module):
         return slots, attn_maps
 
 
-class PSAT(nn.Module):
+class SATv2(nn.Module):
 
     def __init__(
         self,
@@ -272,21 +260,19 @@ class PSAT(nn.Module):
         nn.init.xavier_uniform_(init_log_sigma)
         self.init_log_sigma = nn.Parameter(init_log_sigma.squeeze())
 
-        self.inv_cross_k = nn.Linear(input_dim, slot_dim, bias=ica_bias)
-        self.inv_cross_v = nn.Linear(input_dim, slot_dim, bias=ica_bias)
-        self.inv_cross_q = nn.Linear(slot_dim, slot_dim, bias=ica_bias)
-
-        self.t_encoder = Encoder(
-            dim=input_dim,
-            depth=depth,
-            ff_glu=True,
-            ff_swish=True,
+        self.layers = nn.ModuleList(
+            [
+                SATv2Layer(
+                    input_dim=input_dim,
+                    slot_dim=slot_dim,
+                    ica_bias=ica_bias,
+                    eps=eps,
+                )
+                for _ in range(depth)
+            ]
         )
 
-        self.norm_input = nn.LayerNorm(input_dim)
-        self.norm_prev_slots = nn.LayerNorm(slot_dim)
-        self.norm_slots = nn.LayerNorm(slot_dim)
-        self.norm_ica = nn.LayerNorm(slot_dim)
+        self.norm_input = nn.LayerNorm(slot_dim)
 
     def sample(self, x, n_slots):
         b, _, _ = x.shape
@@ -298,62 +284,89 @@ class PSAT(nn.Module):
 
         return sample
 
-    def inv_cross_attn(self, q, k, v, prev_attn, mask=None):
+    def step(self, slots, x, return_attn=False, mask=None):
 
-        dots = torch.einsum("bid,bjd->bij", q, k) * self.scale
-
-        if mask is not None:
-            # NOTE: Masking attention is sensitive to the chosen value
-            dots.masked_fill_(~mask[:, None, :], -torch.finfo(k.dtype).max)
-
-        attn = dots.softmax(dim=1) + self.eps
-
-        attn = attn / attn.sum(dim=-1, keepdim=True)
-
-        if prev_attn is not None:
-            attn = attn @ prev_attn
-
-        updates = torch.einsum("bjd,bij->bid", v, attn)
-
-        return updates, attn
-
-    def step(self, slots, k, v, prev_attn, return_attn=False, mask=None):
-
-        q = self.inv_cross_q(self.norm_slots(slots))
-
-        updates, attn = self.inv_cross_attn(q, k, v, prev_attn)
-
-        slots = slots + updates
-        slots = self.norm_ica(slots)
-
-        slots = self.t_encoder(slots)
+        for layer in self.layers:
+            slots, attn = layer(slots, x, mask=mask)
 
         if return_attn:
             return slots, attn
 
         return slots
 
-    def forward(self, features, prev_slots, prev_attn=None, n_slots=8, mask=None):
-        if prev_attn is not None:
-            prev_attn = rearrange(prev_attn, "b hw n -> b n hw")
+    def forward(self, x, n_slots=8, mask=None):
 
-        features = self.norm_input(features)
-        prev_slots = self.norm_prev_slots(prev_slots)
+        x = self.norm_input(x)
 
-        init_slots = self.sample(prev_slots, n_slots)
+        init_slots = self.sample(x, n_slots)
         slots = init_slots.clone()
 
-        k = self.inv_cross_k(prev_slots)
-        v = self.inv_cross_v(features)
+        # for _ in range(self.n_iters):
+        #     slots = self.step(slots, x, mask=mask)
+        #
+        # if self.implicit:
+        #     slots = slots.detach() - init_slots.detach() + init_slots
 
-        for _ in range(self.n_iters):
-            slots = self.step(slots, k, v, prev_attn, mask=mask)
-
-        if self.implicit:
-            slots = slots.detach() - init_slots.detach() + init_slots
-
-        slots, attn_map = self.step(slots, k, v, prev_attn, return_attn=True, mask=mask)
+        slots, attn_map = self.step(slots, x, return_attn=True, mask=mask)
 
         attn_map = rearrange(attn_map, "b n hw -> b hw n")
 
-        return slots, attn_map
+        return slots, [attn_map]
+
+
+class SATv2Layer(nn.Module):
+
+    def __init__(
+        self,
+        input_dim,
+        slot_dim,
+        ica_bias=False,
+        eps=1e-8,
+    ):
+        super().__init__()
+        self.in_dim = input_dim
+        self.slot_dim = slot_dim
+        self.eps = eps
+
+        self.scale = input_dim**-0.5
+
+        self.init_mu = nn.Parameter(torch.randn(slot_dim))
+        init_log_sigma = torch.empty((1, 1, slot_dim))
+        nn.init.xavier_uniform_(init_log_sigma)
+        self.init_log_sigma = nn.Parameter(init_log_sigma.squeeze())
+
+        self.inv_cross_k = nn.Linear(input_dim, slot_dim, bias=ica_bias)
+        self.inv_cross_v = nn.Linear(input_dim, slot_dim, bias=ica_bias)
+        self.inv_cross_q = nn.Linear(slot_dim, slot_dim, bias=ica_bias)
+
+        self.t_encoder = Encoder(
+            dim=slot_dim,
+            depth=1,
+            ff_glu=True,
+            ff_swish=True,
+        )
+
+        self.norm_slots = nn.LayerNorm(slot_dim)
+        self.norm_ica = nn.LayerNorm(slot_dim)
+
+    def inv_cross_attn(self, q, k, v, mask=None):
+
+        dots = torch.einsum("bid,bjd->bij", q, k) * self.scale
+        if mask is not None:
+            # NOTE: Masking attention is sensitive to the chosen value
+            dots.masked_fill_(~mask[:, None, :], -torch.finfo(k.dtype).max)
+        attn = dots.softmax(dim=1) + self.eps
+        attn = attn / attn.sum(dim=-1, keepdim=True)
+        updates = torch.einsum("bjd,bij->bid", v, attn)
+
+        return updates, attn
+
+    def forward(self, slots, x, mask=None):
+        q = self.inv_cross_q(self.norm_slots(slots))
+        k = self.inv_cross_k(x)
+        v = self.inv_cross_v(x)
+        updates, attn = self.inv_cross_attn(q, k, v, mask=mask)
+        slots = slots + updates
+        slots = self.norm_ica(slots)
+        slots = self.t_encoder(slots)
+        return slots, attn
